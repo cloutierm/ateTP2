148c210df03c6b35d1ccdaca67c40392775dcc57
cloutierm <cloutierm@educ.college-montreal.qc.ca>
2023-09-27 13:52:57 -0400

    hkuifuireshuivgregev

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Crée par Maximilien Cloutier
-# Crée en 2022
-# Exercice de raffraichissement
+# Crée en 2023
+# Code du TP2
 
 # Importation de random
 import random
@@ -9,23 +9,27 @@ import random
 nombreEssais = 0
 essai = -1
 reponse = True
-inconnu = random.randint(0, 100)
+inconnu = random.randint(0, 1000)
 hs = -1
 
+# Boucles qui va éxecuter la question jusqu'à ce que le nombre est trouvé
 while (reponse):
-    essai = int(input("Entrez un chiffre de 0 à 100 :"))
+    essai = int(input("Entrez un chiffre de 0 à 1000 :"))
 
     nombreEssais += 1
 
+    # Tant que le nombre n'est pas trouvé
     if essai != inconnu:
 
-        print("Mauvaise réponse, vous avez essayé %d fois \n" % (nombreEssais))
-
+        # Si le nombre est plus grand que le nombre recherché
         if essai > inconnu:
-            print("Votre nombre est trop grand.")
+            print("Le nombre recherché est plus petit que", essai)
 
-        else:
-            print("Votre nombre est trop petit.")
+        # Si le nombre est plus petit que le nombre recherché
+        elif essai < inconnu:
+            print("Le nombre recherché est plus grand que", essai,)
 
-    elif essai:
-        print("\n\nBravo!Votre score est : %d" % (nombreEssais))
+    # Si le nombre recherché est trouvé
+    elif essai == inconnu:
+        print("\n\nBravo! Le nombre recherché était", inconnu, "\nVotre score est : %d" % (nombreEssais))
+        reponse = False
