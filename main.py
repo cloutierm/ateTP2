b8f7a9f8926bb3adae8368cace3f91f711eaaf4f
cloutierm <cloutierm@educ.college-montreal.qc.ca>
2023-09-27 14:18:55 -0400

    cfdfhdfkjvshk

--- main.py
+++ main.py
@@ -5,29 +5,29 @@
 # Importation de random
 import random
 
+# Choix des l'intervale
+nbr1 = int(input("Entrer le premier notre de votre intervale: "))
+nbr2 = int(input("Entrer le deuxième nombre de votre intervale: "))
+
 # Variables globales
 nombreEssais = 0
 essai = -1
 reponse = True
-inconnu = random.randint(0, 1000)
-hs = -1
+inconnu = random.randint(nbr1, nbr2)
 
 # Boucles qui va éxecuter la question jusqu'à ce que le nombre est trouvé
 while (reponse):
-    essai = int(input("Entrez un chiffre de 0 à 1000 :"))
+    essai = int(input("Entrez un chiffre de %d et %d: " % (nbr1, nbr2)))
 
     nombreEssais += 1
 
-    # Tant que le nombre n'est pas trouvé
-    if essai != inconnu:
+    # Si le nombre est plus grand que le nombre recherché
+    if essai > inconnu:
+        print("Le nombre recherché est plus petit que", essai)
 
-        # Si le nombre est plus grand que le nombre recherché
-        if essai > inconnu:
-            print("Le nombre recherché est plus petit que", essai)
-
-        # Si le nombre est plus petit que le nombre recherché
-        elif essai < inconnu:
-            print("Le nombre recherché est plus grand que", essai,)
+    # Si le nombre est plus petit que le nombre recherché
+    elif essai < inconnu:
+        print("Le nombre recherché est plus grand que", essai,)
 
     # Si le nombre recherché est trouvé
     elif essai == inconnu:
