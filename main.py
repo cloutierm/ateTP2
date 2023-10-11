e39f540322a869df6d49faae9507c011c7e1a4f8
cloutierm <cloutierm@educ.college-montreal.qc.ca>
2023-10-11 10:07:51 -0400

    xghnxhfngf

--- main.py
+++ main.py
@@ -2,34 +2,53 @@
 # Crée en 2023
 # Code du TP2
 
-# Importation de random
-import random
 
-# Choix des l'intervale
-nbr1 = int(input("Entrer le premier notre de votre intervale: "))
-nbr2 = int(input("Entrer le deuxième nombre de votre intervale: "))
+rejouer = True
 
-# Variables globales
-nombreEssais = 0
-essai = -1
-reponse = True
-inconnu = random.randint(nbr1, nbr2)
 
-# Boucles qui va éxecuter la question jusqu'à ce que le nombre est trouvé
-while (reponse):
-    essai = int(input("Entrez un chiffre de %d et %d: " % (nbr1, nbr2)))
+while (rejouer):
 
-    nombreEssais += 1
+    # Importation de random
+    import random
 
-    # Si le nombre est plus grand que le nombre recherché
-    if essai > inconnu:
-        print("Le nombre recherché est plus petit que", essai)
+    # Variables globales
+    nombreEssais = 0
+    essai = -1
+    reponse = True
 
-    # Si le nombre est plus petit que le nombre recherché
-    elif essai < inconnu:
-        print("Le nombre recherché est plus grand que", essai,)
+    # Choix des l'intervale
+    nbr1 = int(input("Entrer le premier notre de votre intervale: "))
+    nbr2 = int(input("Entrer le deuxième nombre de votre intervale: "))
 
-    # Si le nombre recherché est trouvé
-    elif essai == inconnu:
-        print("\n\nBravo! Le nombre recherché était", inconnu, "\nVotre score est : %d" % (nombreEssais))
-        reponse = False
+    # Choix du nombre aleatoire
+    inconnu = random.randint(nbr1, nbr2)
+
+    # Boucles qui va éxecuter la question jusqu'à ce que le nombre est trouvé
+    while (reponse):
+        essai = int(input("Entrez un chiffre de %d et %d: " % (nbr1, nbr2)))
+
+        nombreEssais += 1
+
+        # Si le nombre est plus grand que le nombre recherché
+        if essai > inconnu:
+            print("Le nombre recherché est plus petit que", essai)
+
+        # Si le nombre est plus petit que le nombre recherché
+        elif essai < inconnu:
+            print("Le nombre recherché est plus grand que", essai,)
+
+        # Si le nombre recherché est trouvé
+        elif essai == inconnu:
+            print("\n\nBravo! Le nombre recherché était", inconnu, "\nVotre score est : %d" % (nombreEssais))
+            reponse = False
+            repeter = str(input("Voulez-vous rejouer? "))
+            if repeter == "oui": # Si la personne veut rejouer
+                rejouer = True
+
+            elif repeter == "non": # Si la personne ne veut pas rejouer
+                rejouer = False
+                print("Bye")
+
+            else:
+                print("Je n'ai pas compris, bye.")
+                rejouer = False
